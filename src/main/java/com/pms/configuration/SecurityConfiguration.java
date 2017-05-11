cfc6232cf1aaa981e75bdf50045044e2d2831f0d
Chabed Alam <chabedalam11@gmil.com>
2017-05-12 00:12:19 +0600

    new login and set data table is t30004

--- src/main/java/com/pms/configuration/SecurityConfiguration.java
+++ src/main/java/com/pms/configuration/SecurityConfiguration.java
@@ -64,7 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 	    web
 	       .ignoring()
-	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**","/bootstrap/**","/fragments/**");
+	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**","/bootstrap/**");
 	}
 
 }
